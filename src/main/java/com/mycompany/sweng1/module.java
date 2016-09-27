0687ab7d52bcbde2bafad4a1f66369e0ced6fccb
David Murphy <david@murphytech.ie>
2016-09-27 22:33:58 +0100

    Added to module class

--- src/main/java/com/mycompany/sweng1/module.java
+++ src/main/java/com/mycompany/sweng1/module.java
@@ -5,11 +5,40 @@
  */
 package com.mycompany.sweng1;
 
+import java.util.*;
+
 /**
  *
  * @author David
  * @Date 23/09/2016
  */
 public class module {
+    private String Module_Name;
+    private String Module_ID;
+    private List<student> Students = new ArrayList<student>();
+    
+    public void setModule_Name(String name){
+        Module_Name = name;
+    }
+
+    public String getModule_Name(){
+        return this.Module_Name;
+    }
+    
+    public void setModule_ID(String id){
+        Module_ID = id;
+    }
+
+    public String getModule_ID(){
+        return this.Module_ID;
+    }
+    
+    public void addStudent(student newStudent){
+        Students.add(newStudent);
+    }
+    
+    public List<student> getStudentList(){
+        return Students;
+    }
     
 }
